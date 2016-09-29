360d4df0687497841b20662cecadf9a8ac087f45
James Lim <james.albert.lim@gmail.com>
2016-09-29 10:09:18 -0700

    favicon heroku

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ var players = require('./api/players.js');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../client/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
